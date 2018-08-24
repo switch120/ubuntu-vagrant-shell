b287ae019991b6d4c823dc5b35be6f9b526e13af
switch120 <switch120@gmail.com>
2018-08-24 16:35:25 -0400

    default mysql password update

--- provisioner.sh
+++ provisioner.sh
@@ -21,7 +21,7 @@ sudo apt-get install libcap2-bin
 sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
 
 #install mySql
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password 1234'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 1234'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password SuperSecretPasswordHere'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password SuperSecretPasswordHere'
 
 apt-get install -y mysql-server
